0bf69384e45c34ab79f4861f4b4c0dbcfc1e4da8
Umair <umairmohd425@gmail.com>
2025-08-24 23:04:37 +0530

    Test CI/CD pipeline

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from decouple import config
 
-# creating variable to store SECRET_KEY
+# creating to store SECRET_KEY
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
